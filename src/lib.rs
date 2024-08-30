9c9e0b0252d4f1df30e508b794885d8f33f819ab
Ioannis Nezis <ioannis@nezis.de>
2024-08-30 12:48:59 +0200

    change init function name to avoid collision

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use server::Server;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn init() -> Server {
+pub fn init_language_server() -> Server {
     #[cfg(feature = "wasm")]
     wasm_logger::init(wasm_logger::Config::default());
     return Server::new();
