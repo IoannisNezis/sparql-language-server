71cd2c7fe51edae5c15ce4f6a4641d8019d6df5c
Ioannis Nezis <ioannis@nezis.de>
2025-02-16 17:41:26 +0100

    TST: add testing queries

--- tests/format_stability_test.rs
+++ tests/format_stability_test.rs
@@ -8,21 +8,15 @@ use tree_sitter::Parser;
 
 #[test]
 fn compare_parse_trees() {
-    let mut unchanged = true;
+    let mut unchanged = 0;
+    let mut changed = 0;
     for entry in fs::read_dir("tests/example-input/").unwrap() {
         let path = entry.unwrap().path();
         let mut file = File::open(path.as_path()).expect("File should be readable");
         let mut content_before = String::new();
         file.read_to_string(&mut content_before).unwrap();
-
-        let output = Command::new("qlue-ls")
-            .arg("format")
-            .arg(path.as_path())
-            .output()
-            .expect("Command \"qlue-ls format\" should execute without error");
-
-        assert!(output.status.success());
-        let content_after = String::from_utf8_lossy(&output.stdout).to_string();
+        let output = qlue_ls::format_raw(content_before.clone()).expect("Formatting should work");
+        let content_after = String::from_utf8_lossy(&output.as_bytes()).to_string();
 
         let mut parser = Parser::new();
         parser
@@ -31,9 +25,16 @@ fn compare_parse_trees() {
         let tree_before = parser.parse(content_before, None).unwrap();
         let tree_after = parser.parse(content_after, None).unwrap();
         if tree_before.root_node().to_string() != tree_after.root_node().to_string() {
-            println!("Querie changed by format: {}", path.to_str().unwrap());
-            unchanged = false;
+            println!("Query changed by format: {}", path.to_str().unwrap());
+            changed += 1;
+        } else {
+            unchanged += 1;
         }
     }
-    assert!(unchanged);
+    println!(
+        "----------RESULT-----------\n{}/{} failed\n-----------------------------",
+        changed,
+        changed + unchanged
+    );
+    assert!(changed == 0);
 }
